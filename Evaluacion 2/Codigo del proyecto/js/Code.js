7b749607db907a83c2643329f4269c81abdccf85
RaulFVH <107163249+RaulFVH@users.noreply.github.com>
2023-05-02 23:09:01 -0400

    Modo nocturno y vis de hora actual

--- Evaluacion 2/Codigo del proyecto/js/Code.js
+++ Evaluacion 2/Codigo del proyecto/js/Code.js
@@ -2,3 +2,19 @@ function cambiarModo() {
     var cuerpoweb = document.body; 
     cuerpoweb.classList.toggle("oscuro"); 
 }
+
+
+function mostrarHora() {
+    const fecha = new Date(); 
+    const hora = fecha.getHours();
+    const minutos = fecha.getMinutes();
+    const segundos = fecha.getSeconds();
+    const horaFormateada = hora < 10 ? "0" + hora : hora;
+    const minutosFormateados = minutos < 10 ? "0" + minutos : minutos;
+    const segundosFormateados = segundos < 10 ? "0" + segundos : segundos;
+    const reloj = document.getElementById("reloj");
+    reloj.textContent = `${horaFormateada}:${minutosFormateados}:${segundosFormateados}`;
+  }
+
+  setInterval(mostrarHora, 1000);
+  
